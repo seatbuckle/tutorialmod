ea062073fda050648379a4a23f20a0d75d9f247d
Seatbuckle <joseefable@gmail.com>
2023-03-15 13:31:50 -0700

    help

--- src/main/java/net/mura/tutorialmod/TutorialMod.java
+++ src/main/java/net/mura/tutorialmod/TutorialMod.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+	//test
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
