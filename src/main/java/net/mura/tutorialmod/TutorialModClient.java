bb0e011fd61a3c45a1aa85730f68312d205ba578
Seatbuckle <joseefable@gmail.com>
2023-03-15 13:28:49 -0700

    help

---
+++ src/main/java/net/mura/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.mura.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mura/tutorialmod/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package net.mura.tutorialmod.mixin;
+
+import net.mura.tutorialmod.TutorialMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.mura.tutorialmod.TutorialMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
